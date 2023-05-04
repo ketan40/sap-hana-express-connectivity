826727df2a57961bc4e335b55a6780bc327e149d
Ketan Deshmukh <deshmukhketan440@gmail.com>
2023-05-04 19:31:47 -0400

    Update expressSqlConnect.js

--- expressSqlConnect.js
+++ expressSqlConnect.js
@@ -16,10 +16,10 @@ const connectionParams = {
          console.log('sql server connected');
         // create Request object
         let request = new sql.Request();
-        const userName = 'kedeshmu'
+        const userName = ''
 
         // query to the database and get the records
-        const queryToBeExecuted = "select * from IBRequests where SourceID = 8 and Username ='kedeshmu' ORDER BY ReqDate desc";
+        const queryToBeExecuted = "select * from IBRequests where SourceID = 8 and Username ='' ORDER BY ReqDate desc";
         console.log(queryToBeExecuted);
         request.query(queryToBeExecuted, function (err, recordset) {
             if (err) {
